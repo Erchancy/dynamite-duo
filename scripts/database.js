0004996b0bfbda448f36e669c6a8abcf31913898
Lance Buckley <erchancy@gmail.com>
2023-04-14 10:05:45 -0500

    Updated Scripts with code

---
+++ scripts/database.js
@@ -0,0 +1,34 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "Syndrome",
+            power: "Super Intelligence"
+        },
+        {
+            id: 2,
+            name: "Ice Age",
+            power:  "Ice Creation"
+        }
+    ]
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
---
+++ scripts/heroes.js
@@ -0,0 +1,30 @@
+import { getHeroes } from "./database.js"
+import { getVillains } from "./database.js"
+
+const heroes = getHeroes()
+
+const villains = getVillains()
+
+export const HeroList = () => {
+    let heroHTML = "<ul>"
+
+    for (const hero of heroes) {
+        heroHTML += `<li>${hero.name}</li>`
+    }
+
+    heroHTML += "</ul>"
+
+    return heroHTML
+}
+
+export const VillainList = () => {
+    let villainHTML = "<ul>"
+
+    for (const villain of villains) {
+        villainHTML += `<li>${villain.name}</li>`
+    }
+
+    villainHTML += "</ul>"
+
+    return villainHTML
+}
